246438ac03f1295deee59eff428e1056e5b66097
vanysul <sulsyvany@gmail.com>
2024-12-01 21:21:36 +0100

    update servlet

---
+++ src/main/java/by/sulai/intership/projects/passwordmanager/web/AuthFilter.java
@@ -0,0 +1,38 @@
+package by.sulai.intership.projects.passwordmanager.web;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class AuthFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+
+        String authHeader = httpRequest.getHeader("AUTH");
+        if (authHeader == null || !authHeader.equals("Иван")) {
+            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized: Invalid AUTH header");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        // Очистка ресурсов
+    }
+
+}
---
+++ src/main/java/by/sulai/intership/projects/passwordmanager/web/HelloWorldServlet.java
@@ -0,0 +1,17 @@
+package by.sulai.intership.projects.passwordmanager.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        response.getWriter().write("Hello, World!");
+    }
+
+}
